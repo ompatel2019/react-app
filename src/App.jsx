7b418f6fa6e30096d00d338b5f65b63f3fb86f9d
Om Patel <ompatel2019@gmail.com>
2024-07-17 08:08:37 +1000

    Routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import React from 'react';
-import NavBar from './components/Navbar';
-import Hero from './components/Hero';
-import HomeCards from './components/HomeCards';
-import JobsListings from './components/JobsListings';
-import ViewAllJobs from './components/ViewAllJobs';
+import HomePage from './pages/HomePage';
+import MainLayout from './layouts/MainLayout';
+import JobsPage from './pages/JobsPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path='jobs' element={<JobsPage />} />
+      <Route path='*' element={<NotFoundPage />} />
+    </Route>
+  )
+);
 
 const App = () => {
-  return (
-    <>
-      <NavBar />
-      <Hero />
-      <HomeCards />
-      <JobsListings />
-      <ViewAllJobs />
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+// src/components/Footer.jsx
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-indigo-700 text-white py-4">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <p className="text-center">© 2024 React Jobs. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
